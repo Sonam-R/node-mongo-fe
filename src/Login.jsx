54f05330614004dbc1ada460d98ce9ead63fcb2c
Sonam <sonam.raturi44@gmail.com>
2025-03-18 22:43:23 +0530

    login api integrated

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,20 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import axios from 'axios';
 
 const Login = () => {
+  const [emailId, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const handleLogin = () => {
+    try{
+      const response = axios.post("http://localhost:7777/login", {
+        emailId,
+        password
+      })
+
+    } catch(err){
+      console.log(err);
+    }
+  }
   return (
     <>
       <div className="card mx-auto m-50 flex max-w-sm shadow-sm">
         <div className="card-body">
           <fieldset className="fieldset">
-            <legend className="fieldset-legend">Email</legend>
-            <input type="text" className="input" placeholder="Type here" />
+            <legend className="fieldset-legend">Email ID</legend>
+            <input type="text" value={emailId} onChange={(e) => setEmail(e.target.value)} className="input" placeholder="Type here" />
           </fieldset>
           <fieldset className="fieldset">
             <legend className="fieldset-legend">Password</legend>
-            <input type="password" className="input" placeholder="Type here" />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Type here" />
           </fieldset>
           <div className="justify-end card-actions">
-            <button className="btn btn-primary">Submit</button>
+            <button onClick={handleLogin} className="btn btn-primary">Login</button>
           </div>
         </div>
       </div>
